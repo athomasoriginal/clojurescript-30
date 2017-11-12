5ae53f5f91fba63e09512b0fecf7f12ca621dd8f
tkjone <thomasmattacchione@gmail.com>
2017-11-12 12:17:03 -0500

    issue #34 Init events module

--- 34-calendar-fun/src/clj/calendar_fun/core.clj
+++ 34-calendar-fun/src/clj/calendar_fun/core.clj
@@ -1,7 +1,44 @@
 (ns calendar-fun.core
   (:gen-class))
 
+
+;; Helpers
+
+(defn events?
+  "Check if `events` vector contains more than one event."
+  [events]
+  (> (count events) 1))
+
+
+(defn compare-events
+  "Compare second events start time to the first events end time.
+  When second event start time is less than first event end time this results
+  in a conflict.  Conflict returns:  `[ [1 4] [2 3] ]`.  No conflicts returns
+  nil"
+  [x y]
+  (let [[s1 e1] x
+        [s2 e2] y]
+      (when (< s2 e1)
+        (conj [] x y))))
+
+
+;; Core
+
+(defn find-event-conflicts
+  "Find all pairs of calendar events that conflict with one another.
+  When conficts are found, this returns a list of vectors [ [ [1 3] [2 4] ] ... ].
+  When no conflicts are found, return empty list."
+  [events]
+  (when events?
+    (remove nil?
+      (for [[x :as xs] (take (count (sort events)) (iterate next (sort events)))
+             y xs
+             :when (not= x y)]
+        (compare-events x y)))))
+
+
+;; Start
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Here comes the fun!"))
+  "Ya double-booked, son!"
+  [& args])
