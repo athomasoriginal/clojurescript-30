49681035e022c24aef47879d6c73f3e4a0aae622
tkjone <thomasmattacchione@gmail.com>
2017-12-10 11:25:50 -0500

    issue #13 Allow user to specify an optional ID add-event

--- 34-calendar-fun/src/cljs/day_calendar/actions.cljs
+++ 34-calendar-fun/src/cljs/day_calendar/actions.cljs
@@ -5,9 +5,9 @@
   "Return `event` data type.
   https://www.lvguowei.me/post/named-arguments-in-clojure/
   https://clojure.org/guides/destructuring#_keyword_arguments"
-  [& {:keys [name start-time end-time]}]
+  [& {:keys [id name start-time end-time]}]
   {
-    :id         (random-uuid)
+    :id         (if id id (random-uuid))
     :name       name
     :start-time start-time
     :end-time   end-time})
