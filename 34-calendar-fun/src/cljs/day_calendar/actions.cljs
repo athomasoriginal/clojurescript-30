aa4dcd293609e035d67d3925d82fe6c554923472
tkjone <thomasmattacchione@gmail.com>
2017-12-10 18:20:25 -0500

    issue #13 Set default value using destructuring

--- 34-calendar-fun/src/cljs/day_calendar/actions.cljs
+++ 34-calendar-fun/src/cljs/day_calendar/actions.cljs
@@ -5,9 +5,9 @@
   "Return `event` data type.
   https://www.lvguowei.me/post/named-arguments-in-clojure/
   https://clojure.org/guides/destructuring#_keyword_arguments"
-  [& {:keys [id name start-time end-time]}]
+  [& {:keys [id name start-time end-time] :or {id (random-uuid)}}]
   {
-    :id         (if id id (random-uuid))
+    :id         id
     :name       name
     :start-time start-time
     :end-time   end-time})
