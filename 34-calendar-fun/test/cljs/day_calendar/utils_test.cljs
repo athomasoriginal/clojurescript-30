f650c1a291351c6877c0b134164a45e05330b779
tkjone <thomasmattacchione@gmail.com>
2017-12-10 11:27:07 -0500

    issue #13 Remove find-conflict tests
    Removed becayse find-conflicts is already testing these.  Duplicating
    efforts

--- 34-calendar-fun/test/cljs/day_calendar/utils_test.cljs
+++ 34-calendar-fun/test/cljs/day_calendar/utils_test.cljs
@@ -7,16 +7,19 @@
 
 ;; Fixtures
 
-(def with-conflicts [(add-event :name "1" :start-time 1 :end-time 3)
-                     (add-event :name "2" :start-time 2 :end-time 4)
-                     (add-event :name "3" :start-time 1 :end-time 5)])
+(def with-conflicts [(add-event :id "36743" :name "1" :start-time 1 :end-time 3)
+                     (add-event :id "36744" :name "2" :start-time 2 :end-time 4)
+                     (add-event :id "36745" :name "3" :start-time 1 :end-time 5)])
 
-(def another-with-conflicts [(add-event :name "1" :start-time 1 :end-time 2)
-                             (add-event :name "2" :start-time 1 :end-time 2)])
+(def another-with-conflicts [(add-event :id "36743" :name "1" :start-time 1 :end-time 2)
+                             (add-event :id "36744" :name "2" :start-time 1 :end-time 2)])
 
-(def without-conflicts [(add-event :name "1" :start-time 1 :end-time 3)
-                        (add-event :name "2" :start-time 4 :end-time 5)
-                        (add-event :name "3" :start-time 6 :end-time 7)])
+(def same-events-should-be-ignored [(add-event :id "36744" :name "1" :start-time 1 :end-time 2)
+                                    (add-event :id "36744" :name "1" :start-time 1 :end-time 2)])
+
+(def without-conflicts [(add-event :id "36743" :name "1" :start-time 1 :end-time 3)
+                        (add-event :id "36744" :name "2" :start-time 4 :end-time 5)
+                        (add-event :id "36745" :name "3" :start-time 6 :end-time 7)])
 
 (def empty-conflict-array [])
 
@@ -25,21 +28,16 @@
 
 (deftest find-conflicts-test
   (testing "events with conflicts"
-    (is (= (find-conflicts with-conflicts) '([36743 36743] [36743 36743] [36743 36743]))))
+    (is (= (find-conflicts with-conflicts) '(["36743" "36745"] ["36743" "36744"] ["36745" "36744"]))))
 
   (testing "events with conflicts - events at the exact same time"
-    (is (= (find-conflicts another-with-conflicts) '([36743 36743]))))
+    (is (= (find-conflicts another-with-conflicts) '(["36743" "36744"]))))
+
+  (testing "no conflicts - events should be ignored"
+    (is (= (find-conflicts same-events-should-be-ignored) '())))
 
   (testing "events without conflicts"
     (is (= (find-conflicts without-conflicts) '())))
 
   (testing "empty event vector"
     (is (= (find-conflicts empty-conflict-array) '()))))
-
-
-(deftest find-conflict-test
-  (testing "events should not conflict"
-    (is (= (find-conflict (add-event :name "1" :start-time 1 :end-time 3) (add-event :name "1" :start-time 5 :end-time 6)) nil)))
-
-  (testing "events should conflict"
-    (is (= (find-conflict (add-event :name "1" :start-time 1 :end-time 3) (add-event :name "2" :start-time 2 :end-time 4)) [36743 36743]))))
