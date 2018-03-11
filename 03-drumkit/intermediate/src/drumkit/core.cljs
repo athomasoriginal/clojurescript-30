54a57fc2a2152a99a97e7399c805e0bd14abb584
tkjone <thomasmattacchione@gmail.com>
2018-03-11 19:43:06 -0400

    03-drumkit - replace gstr with str
    No need for an extra dependency, especially now that I am not really
    trying to explore the different str formatting options

--- 03-drumkit/intermediate/src/drumkit/core.cljs
+++ 03-drumkit/intermediate/src/drumkit/core.cljs
@@ -1,6 +1,5 @@
 ;; create main project namespace
 (ns drumkit.core
-  (:require        [goog.string :as gstr] goog.string.format)
   (:require-macros [drumkit.macros :refer [p pp]]))
 
 
@@ -8,14 +7,15 @@
 
 (defn handle-key-press [e]
   (let [key-code      (.-keyCode e)
-        data-selector (gstr/format "audio[data-key=\"%s\"]" key-code)
-        key-selector  (gstr/format ".key[data-key=\"%s\"]" key-code)
+        data-selector (str "audio[data-key=" "\"" key-code "\"" "]")
+        key-selector  (str ".key[data-key=" "\"" key-code "\"" "]" )
         audio-element (.querySelector js/document data-selector)
         key-element   (.querySelector js/document key-selector)]
     (when-not (.isNull js/goog audio-element)
         (set! (.-currentTime audio-element) 0)
         (.play audio-element)
         (.classList/add key-element "playing"))))
+
 
 (defn handle-remove-transition [e]
   (this-as this
