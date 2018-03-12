748aec4319e258fc9b58657a56050dc6a70a79d0
tkjone <thomasmattacchione@gmail.com>
2018-03-12 19:22:39 -0400

    03-drumkit - indent param vectors
    This is a style choice I like to adhere to.

--- 03-drumkit/intermediate/src/drumkit/core.cljs
+++ 03-drumkit/intermediate/src/drumkit/core.cljs
@@ -5,22 +5,24 @@
 
 ;; Event Handlers
 
-(defn play-sound [e]
-  (let [key-code      (.-keyCode e)
-        data-selector (str "audio[data-key=" "\"" key-code "\"" "]")
-        key-selector  (str ".key[data-key=" "\"" key-code "\"" "]")
-        audio-element (.querySelector js/document data-selector)
-        key-element   (.querySelector js/document key-selector)]
-    (when-not (.isNull js/goog audio-element)
-        (set! (.-currentTime audio-element) 0)
-        (.play audio-element)
-        (.classList/add key-element "playing"))))
+(defn play-sound 
+ [e]
+ (let [key-code      (.-keyCode e)
+       data-selector (str "audio[data-key=" "\"" key-code "\"" "]")
+       key-selector  (str ".key[data-key=" "\"" key-code "\"" "]")
+       audio-element (.querySelector js/document data-selector)
+       key-element   (.querySelector js/document key-selector)]
+   (when-not (.isNull js/goog audio-element)
+       (set! (.-currentTime audio-element) 0)
+       (.play audio-element)
+       (.classList/add key-element "playing"))))
 
 
-(defn remove-transition [e]
-  (this-as this
-    (when (= (.-propertyName e) "transform")
-      (.remove (.-classList this)  "playing"))))
+(defn remove-transition 
+ [e]
+ (this-as this
+   (when (= (.-propertyName e) "transform")
+     (.remove (.-classList this)  "playing"))))
 
 
 ;; Init
