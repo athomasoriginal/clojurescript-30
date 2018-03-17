258fe316c67f148838968be27a5f34714811d3eb
tkjone <thomasmattacchione@gmail.com>
2018-03-17 14:24:30 -0400

    04-clock remove global selectors
    I am not convinced that using the global selectors makes the code
    cleaner to read, because truly, that would be one of the bigger
    benefits.  In addition, after reading Elements of Clojure, this might
    not be the best use of `def`

--- 04-clock/src/app/core.cljs
+++ 04-clock/src/app/core.cljs
@@ -10,12 +10,6 @@
 
 (def everyHour 3600000)
 
-(def second-hand (.querySelector js/document ".hour-hand"))
-
-(def minute-hand (.querySelector js/document ".min-hand"))
-
-(def hour-hand (.querySelector js/document ".second-hand"))
-
 
 ;; Event handlers
 
@@ -23,25 +17,24 @@
   (let [now (js/Date.)
         seconds (.getSeconds now)
         secondsAsDegrees (+ (* (/ seconds 60) 360) 90)
-        transformVal (str "rotate(" secondsAsDegrees "deg)" )]
-    (set! (.-transform (.-style second-hand)) transformVal)))
+        transformVal (str "rotate(" secondsAsDegrees "deg)")]
+    (set! (.-transform (.-style (.querySelector js/document ".hour-hand"))) transformVal)))
 
 
 (defn set-minute-hand []
   (let [now (js/Date.)
         minutes (.getMinutes now)
         minutesAsDegrees (+ (* (/ minutes 60) 360) 90)
-        transformVal (str "rotate(" minutesAsDegrees "deg)" )]
-    (set! (.-transform (.-style minute-hand)) transformVal)))
+        transformVal (str "rotate(" minutesAsDegrees "deg)")]
+    (set! (.-transform (.-style (.querySelector js/document ".min-hand"))) transformVal)))
 
 
 (defn set-hour-hand []
   (let [now (js/Date.)
         hours (.getHours now)
         hoursAsDegrees (+ (* (/ hours 12) 360) 90)
-        transformVal (str "rotate(" hoursAsDegrees "deg)" )]
-    (set! (.-transform (.-style hour-hand)) transformVal)))
-
+        transformVal (str "rotate(" hoursAsDegrees "deg)")]
+    (set! (.-transform (.-style (.querySelector js/document ".second-hand"))) transformVal)))
 
 
 ;; start
