de985ac48d752ec8b53bae6f232c0e31bc610045
tkjone <thomasmattacchione@gmail.com>
2018-03-17 15:21:44 -0400

    04-clock - Remove duplicated efforts

--- 04-clock/src/app/core.cljs
+++ 04-clock/src/app/core.cljs
@@ -6,30 +6,40 @@
 
 (def duration {:sec 1000 :min 60000 :hour 3600000 })
 
+;; Helpers
+
+(defn- set-transform-style!
+  "Sets the transofrm style of the specified hand
+  
+  hand          - type: string - choice of: .hour-hand | .min-hand | .sec-hand
+  transform-val - type: int."
+  [hand transform-val]
+  (set! (.-transform (.-style (.querySelector js/document hand))) transform-val))
+
 ;; Event handlers
 
 (defn set-seconds-hand []
   (let [now (js/Date.)
-        seconds (.getSeconds now)
+        seconds          (.getSeconds now)
         secondsAsDegrees (+ (* (/ seconds 60) 360) 90)
-        transformVal (str "rotate(" secondsAsDegrees "deg)")]
-    (set! (.-transform (.-style (.querySelector js/document ".hour-hand"))) transformVal)))
+        transform-val    (str "rotate(" secondsAsDegrees "deg)")]
+    (set-transform-style! ".hour-hand" transform-val)))
 
 
 (defn set-minute-hand []
   (let [now (js/Date.)
         minutes (.getMinutes now)
         minutesAsDegrees (+ (* (/ minutes 60) 360) 90)
-        transformVal (str "rotate(" minutesAsDegrees "deg)")]
-    (set! (.-transform (.-style (.querySelector js/document ".min-hand"))) transformVal)))
+        transform-val (str "rotate(" minutesAsDegrees "deg)")]
+    (set-transform-style! ".min-hand" transform-val)))
 
 
 (defn set-hour-hand []
   (let [now (js/Date.)
         hours (.getHours now)
         hoursAsDegrees (+ (* (/ hours 12) 360) 90)
-        transformVal (str "rotate(" hoursAsDegrees "deg)")]
-    (set! (.-transform (.-style (.querySelector js/document ".second-hand"))) transformVal)))
+        transform-val (str "rotate(" hoursAsDegrees "deg)")]
+    (set-transform-style! ".sec-hand" transform-val)))
 
 
 ;; start
