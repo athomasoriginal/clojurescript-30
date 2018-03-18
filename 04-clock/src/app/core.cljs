803296bf4f3b3d9ac38d74e12e7609eef814941c
tkjone <thomasmattacchione@gmail.com>
2018-03-18 13:07:26 -0400

    04-clock - remove duplicated functions
    There was a lot of duplication throughout the code, so I removed it and
    also resolved the issue where when the app loaded, all the hands would
    not be properly set until after a certain point.  This is a set further
    which a dev could go and it may not be worth it, but it was a good
    exercise in reusability.

--- 04-clock/src/app/core.cljs
+++ 04-clock/src/app/core.cljs
@@ -4,46 +4,71 @@
 
 ;; globals
 
-(def duration {:sec 1000 :min 60000 :hour 3600000 })
+(def duration 
+ {:sec 1000 
+  :min 60000 
+  :hour 3600000})
+
+(def time-unit->class 
+ {:sec ".second-hand"
+  :min ".min-hand"
+  :hour ".hour-hand"})
 
 ;; Helpers
 
 (defn- set-transform-style!
-  "Sets the transofrm style of the specified hand
+  "Sets the transform-style css property of the specified hand.
   
-  hand          - type: string - choice of: .hour-hand | .min-hand | .sec-hand
+  css-class     - type: string - choice of :sec | :min | :hour
   transform-val - type: int."
-  [hand transform-val]
-  (set! (.-transform (.-style (.querySelector js/document hand))) transform-val))
-
-;; Event handlers
-
-(defn set-seconds-hand []
-  (let [now (js/Date.)
-        seconds          (.getSeconds now)
-        secondsAsDegrees (+ (* (/ seconds 60) 360) 90)
-        transform-val    (str "rotate(" secondsAsDegrees "deg)")]
-    (set-transform-style! ".hour-hand" transform-val)))
+  [css-class transform-val]
+  (set! (.-transform (.-style (.querySelector js/document css-class))) transform-val))
 
 
-(defn set-minute-hand []
-  (let [now (js/Date.)
-        minutes (.getMinutes now)
-        minutesAsDegrees (+ (* (/ minutes 60) 360) 90)
-        transform-val (str "rotate(" minutesAsDegrees "deg)")]
-    (set-transform-style! ".min-hand" transform-val)))
+(defn duration->degrees
+  "Converts the duration provided into a degree.
+
+  duration-type - type: key - choice of :sec | :min | :hour
+  duration      - type: int."
+  [duration-type duration]
+  (cond 
+    (or (= duration-type :sec) (= duration-type :min))
+    (+ (* (/ duration 60) 360) 90)
+
+    (= duration-type :hour)
+    (+ (* (/ duration 12) 360) 90)
+
+    :else
+    nil))
 
 
-(defn set-hour-hand []
-  (let [now (js/Date.)
-        hours (.getHours now)
-        hoursAsDegrees (+ (* (/ hours 12) 360) 90)
-        transform-val (str "rotate(" hoursAsDegrees "deg)")]
-    (set-transform-style! ".sec-hand" transform-val)))
+(defn get-time
+  "Returns the time-unit based on the current time provided."
+  [now time-unit]
+  (cond
+    (= time-unit :sec) (.getSeconds now)
+    (= time-unit :min) (.getMinutes now)
+    :else              (.getHours now)))
 
 
-;; start
+(defn set-hand! 
+  [time-unit]
+  (let [now           (js/Date.)
+        time          (get-time now time-unit)
+        degrees       (duration->degrees time-unit time)
+        transform-val (str "rotate(" degrees "deg)")
+        css-class     (time-unit->class time-unit)]
+    (set-transform-style! css-class transform-val)))
 
-(js/setInterval #(set-seconds-hand) (:second duration))
-(js/setInterval #(set-minute-hand) (:minute duration))
-(js/setInterval #(set-hour-hand) (:hour duration))
+
+;; Start
+
+; set all clock hands to the correct positions when app loads
+(set-hand! :sec)
+(set-hand! :min)
+(set-hand! :hour)
+
+; update clock hands at 1 sec, min and hour intervals
+(js/setInterval #(set-hand! :sec) (:sec duration))
+(js/setInterval #(set-hand! :min) (:min duration))
+(js/setInterval #(set-hand! :hour) (:hour duration))
