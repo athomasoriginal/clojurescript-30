5e12e60e6acd9c5bf5eecc447abfab6fd6efe814
tkjone <thomasmattacchione@gmail.com>
2018-03-19 19:42:40 -0400

    05-css-variables - replace gstr with str

--- 05-css-variables/src/css_variables/core.cljs
+++ 05-css-variables/src/css_variables/core.cljs
@@ -1,15 +1,14 @@
 (ns css-variables.core
-  (:require        [goog.string :as gstr] goog.string.format)
   (:require-macros [css-variables.macros :refer [p pp]]))
 
 
 (defn handle-update []
   (this-as this
-    (let [suffix (.. this -dataset -sizing)
-          name (.. this -name)
-          val (.. this -value)
-          formatted-name (gstr/format "--%s" name)
-          formatted-value (gstr/format "%s%s" val suffix)]
+    (let [suffix          (.. this -dataset -sizing)
+          name            (.. this -name)
+          val             (.. this -value)
+          formatted-name  (str "--" name)
+          formatted-value (str val suffix)]
       (.. js/document -documentElement -style (setProperty formatted-name formatted-value)))))
 
 ;; Start app
