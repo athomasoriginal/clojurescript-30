26ff9a6d272d02ba7a61c1ef0c89104ef2b576ec
tkjone <thomasmattacchione@gmail.com>
2018-04-04 20:54:52 -0400

    Refactor select-checkbox logic into its own function
    The logic was a little too lengthy to be in an anonymous function.  This
    will lead to readability, mainentance and testing improvements

--- 12-check-multiple-checkboxes/src/multiple_checkboxes/core.cljs
+++ 12-check-multiple-checkboxes/src/multiple_checkboxes/core.cljs
@@ -6,46 +6,54 @@
 (def app-state (atom { :last-checked nil}))
 
 
-;; functions to help us get DOM elements
+;; Functions to help us get DOM elements
 (defn get-checkboxes []
-  (.querySelectorAll js/document ".inbox input[type=\"checkbox\"]"))
+  (.. js/document (querySelectorAll ".inbox input[type=\"checkbox\"]")))
 
 
-;; protocol
+;; Protocol
 (extend-type js/NodeList
    ISeqable
    (-seq [node-list] (array-seq node-list)))
 
 
+;; Helper
+(defn select-checkbox?
+  [in-between? checkbox]
+  (cond
+    ;; 1. checkbox user shift-clicked
+    (identical? checkbox this)
+    (do
+      (set! (.. checkbox -checked) true)
+      (not in-between?))
+
+    ;; 2. checkbox last checked
+    (identical? (get @app-state :last-checked) checkbox)
+    (do
+      (set! (.. checkbox -checked) true)
+      (not in-between?))
+
+    ;; 3. in between the last checked and shift-clicked checkbox
+    (true? in-between?)
+    (do
+      (set! (.. checkbox -checked) true)
+      true)
+
+    ;; 4.  outside of last checked and shift-clicked checkbox
+    :else false))
+
+
+
+;; Event Handler
+
 (defn handle-check [e]
   (this-as this
     (when (and (.. e -shiftKey) (.. this -checked))
-      (reduce (fn [in-between? checkbox]
-                  (cond
-                    ;; 1. checkbox user shift-clicked
-                    (identical? checkbox this)
-                    (do
-                      (set! (.. checkbox -checked) true)
-                      (not in-between?))
-
-                    ;; 2. checkbox last checked
-                    (identical? (get @app-state :last-checked) checkbox)
-                    (do
-                      (set! (.. checkbox -checked) true)
-                      (not in-between?))
-
-                    ;; 3. in between the last checked and shift-clicked checkbox
-                    (true? in-between?)
-                    (do
-                      (set! (.. checkbox -checked) true)
-                      true)
-
-                    ;; 4.  outside of last checked and shift-clicked checkbox
-                    :else false))
-              false
-              (get-checkboxes)))
+      (reduce select-checkbox? false (get-checkboxes)))
     (swap! app-state assoc :last-checked this)))
 
 
-(doseq [checkbox (get-checkboxes)]
+;; start
+
+(doseq [checkbox get-checkboxes]
   (.addEventListener checkbox "click" handle-check))
