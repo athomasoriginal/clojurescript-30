527758398ff9809c11ae6f61d824a1f0c58a3977
tkjone <thomasmattacchione@gmail.com>
2018-06-14 20:53:52 -0400

    Music chord initial commit

---
+++ 39-music-chord-v2/src/app/core.cljs
@@ -0,0 +1,93 @@
+(ns app.core
+  "Demo app illustrating a music guitar chord."
+  (:require hiccups.runtime)
+  (:require-macros
+    [app.macros   :refer [p pp]]
+    [hiccups.core :refer [html]]))
+
+;; Globals
+;; -----------------------------------------------------------------------------
+
+
+(def chord-boxes
+  {:em [[1 :mute] [2 3 1] [3 2 2] [4 :open] [5 1 1] [6 :open]]})
+
+
+;; Components
+;; -----------------------------------------------------------------------------
+
+
+(defn muted-fret?
+  "Returns true when the key provided is :mute"
+  [fret]
+  (= fret :mute))
+
+
+(defn open-fret?
+  "Returns true when the key provided is :open"
+  [fret]
+  (= fret :open))
+
+
+(defn string-css-class
+  "Returns a string representing a CSS class.
+
+  Possible return values:  mute, open, finger"
+  [first-fret? fret curr-fret]
+  (cond
+    ;; because of how we are styling the html, we only want the mutes and
+    ;; opens to be rendered on the first fret.  Reason being them strings are
+    ;; the grid gaps
+    (and first-fret? (muted-fret? fret))
+    "mute"
+
+    (and first-fret? (open-fret? fret))
+    "open"
+
+    (muted-fret? fret)
+    ""
+
+    (open-fret? fret)
+    ""
+
+    (= curr-fret fret)
+    "finger"
+
+    :default
+    ""))
+
+
+(defn fret
+  "Renders 1 fret and 6 strings per fret"
+  [chord curr-fret]
+  (map
+    (fn [chord*]
+      (let [first-fret?               (= curr-fret 1)
+            [string fret* finger-pos] chord*]
+        [:div {:class (str "string " (string-css-class first-fret? fret* curr-fret))}]))
+    chord))
+
+
+(defn frets
+  "Render 4 frets"
+  [chord total-frets]
+  (for [curr-fret total-frets]
+    [:div.fret
+      (fret chord curr-fret)]))
+
+
+(defn chord-box
+  "Render a chord diagram of 4 frets and 6 strings"
+  [title chord]
+  (html [:div.chord-diagram-wrapper
+          [:h1 title]
+          [:div.chord-diagram
+            (frets chord (range 1 5))]]))
+
+
+;; Start
+;; -----------------------------------------------------------------------------
+
+(doto
+  (.. js/document (getElementById "chord-container"))
+  (-> .-innerHTML (set! (chord-box "C Major" (:em chord-boxes)))))
