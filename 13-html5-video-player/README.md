d572772ccb9153a86c274ee2a316e18e39ea0c80
tkjone <thomasmattacchione@gmail.com>
2018-07-14 11:38:00 -0400

    Initial commit 13-html5-video-player

---
+++ 13-html5-video-player/README.md
@@ -0,0 +1,69 @@
+# Custom HTML5 Video Player
+
+Customize the HTML5 Video Player with CLJS.
+
+- [Quickstart](#quickstart)
+- [Learnings](#Learnings)
+  - [Ternary v. If](#ternary-v-if)
+  - [Imperative v. Functional](#imperative-v-functional)
+  - [JS Hot Reloading](#js-hot-reloading)
+
+## Quickstart
+
+- Build and watch the project
+
+  ```bash
+  boot dev
+  ```
+
+Visit site at `http://localhost:3000`
+
+## Learnings
+
+### Ternary V If
+
+In Clojure, the idiomatic way of getting this functionality is an If statement.
+
+## Imperative v Functional
+
+I was required to write this:
+
+```clojure
+(defn toggle-play []
+  (if (.-paused video)
+    (.play video)
+    (.pause video)))
+```
+
+And as I was looking at it I could not help but think that this can't be the best way. The issue I see is that this function:
+
+- Accesses stuff from the global namespace - is there a way to not do this, or is it okay? Not everything has to be reusable, but this just does not feel functional.
+
+This brings up some bigger questions like:
+
+- When should I be using side effects and how can I know if I am using them too often or not enough?
+- When is something just not functional?
+- When should something be pure?
+- Are we supposed to be separating functions in some way that have side effects?
+
+Part of the answer to this is knowing that not everything has to be reusable and that you are building programs so there are going to be domain specific things as part of this. If this is the case, we can start to make it data structure oriented and then the functions are acting on these data structures
+
+One better way for the above might be to just
+
+```clojure
+(map paused? ("play", "pause")) ;;returns true or false and than based on this we toggle the play or not
+```
+
+The above seems nicer, aside from the fact that map is not correct, because we now have a function that only cares if it finds a true or false in our domain of the video player app. I was inspired to do the above in the [predicates](https://github.com/mynomoto/lt-clojure-tutorial/blob/master/conditionals.clj) section of the above
+
+### JS Hot Reloading
+
+If you are running this app and live coding it you are going to see that hot reloading is not really working. This is because to make something hot reloadable you have to write your code to be hot reloadable.
+
+# Musings
+
+I am keeping these around because I had issues with these so others might as well and they could be good topics to discuss in the future.
+
+- What do people like when it comes to `this-as`? From a style convention or a when to use perspective?
+- At which point do you make the anonymous function its own named function?
+- when grabbing DOM elements, which one would be preferred `defn` or `def`
---
+++ 13-html5-video-player/src/html5_video_player/core.cljs
@@ -0,0 +1,109 @@
+;; create main project namespace
+(ns html5-video-player.core
+  (:require        [goog.string :as gstr] goog.string.format)
+  (:require-macros [html5-video-player.macros :refer [p pp]]))
+
+;; constants
+
+(def player (.querySelector js/document ".player"))
+
+(def video (.querySelector player ".viewer"))
+
+(def toggle (.querySelector player ".toggle"))
+
+(def progress (.querySelector player ".progress"))
+
+(def progress-bar (.querySelector player ".progress__filled"))
+
+(def skip-buttons (.querySelectorAll player "[data-skip]"))
+
+(def ranges (.querySelectorAll player ".player__slider"))
+
+;; state
+
+(def app-state (atom {:mouse-down? false}))
+
+;; protocols
+
+(extend-type js/NodeList
+   ISeqable
+   (-seq [node-list] (array-seq node-list)))
+
+
+;; event functions
+
+(defn toggle-mouse-down []
+  (let [current-mouse-down (get @app-state :mouse-down?)]
+    (swap! app-state assoc :mouse-down? (not current-mouse-down))))
+
+
+(defn toggle-play [e]
+  (if (.-paused video)
+    (.play video)
+    (.pause video)))
+
+
+(defn toggle-play-btn-icon [e]
+  (this-as this
+    (let [icon (if (.-paused this) "►" "❚ ❚")]
+      (set! (.-textContent toggle) icon))))
+
+
+(defn skip [e]
+  (this-as this
+    (let [skip-count      (.. this -dataset -skip)
+          skip-count-str  (js/parseFloat skip-count)
+          next-skip-count (+ (.-currentTime video) skip-count-str)]
+      (set! (.-currentTime video) next-skip-count))))
+
+
+(defn handle-range-update [e]
+  (this-as this
+    (let [volume-slider? (= (.-name this) "volume")
+          playback-rate? (= (.-name this) "playbackRate")]
+      (when volume-slider?
+        (set! (.-volume video) (.-value this)))
+      (when playback-rate?
+        (set! (.-playbackRate this) (.-value this))))))
+
+
+(defn handle-progress [e]
+  (let [percent         (* (/ (.-currentTime video) (.-duration video)) 100)
+        next-flex-basis (str percent "%")]
+      (p next-flex-basis)
+    (set! (.. progress-bar -style -flexBasis) next-flex-basis)))
+
+
+(defn scrub [e]
+    (let [scrub-time (* (/ (.-offsetX e) (.-offsetWidth progress)) (.-duration video))]
+      (set! (.-currentTime video) scrub-time)))
+
+
+;; event listeners
+
+(.addEventListener video "click" toggle-play)
+
+(.addEventListener video "play" toggle-play-btn-icon)
+
+(.addEventListener video "pause" toggle-play-btn-icon)
+
+(.addEventListener video "timeupdate" handle-progress)
+
+(.addEventListener progress "click" scrub)
+
+(.addEventListener progress "mousemove" #(and (get @app-state :mouse-down?) scrub))
+
+(.addEventListener progress "mousedown" toggle-mouse-down)
+
+(.addEventListener progress "mouseup" toggle-mouse-down)
+
+(.addEventListener toggle "click" toggle-play)
+
+(doseq [btn skip-buttons]
+  (.addEventListener btn "click" skip))
+
+(doseq [range ranges]
+  (.addEventListener range "change" handle-range-update))
+
+(doseq [range ranges]
+  (.addEventListener range "mousemove" handle-range-update))
